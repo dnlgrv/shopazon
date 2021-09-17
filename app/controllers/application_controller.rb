f5884a3e7e9266ed5c071ae6bf171d1d611acbe0
Daniel Grieve <dnlgrv@hey.com>
2021-09-17 12:53:26 +0100

    Add auto-created cart for every visitor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_cart
+  before_action :create_cart, unless: -> { Current.cart.present? }
+
+  private
+    def set_current_cart
+      if session[:cart_id].present? && cart = Cart.find_by(id: session[:cart_id])
+        Current.cart = cart
+      end
+    end
+
+    def create_cart
+      cart = Cart.create!
+      session[:cart_id] = cart.id
+      Current.cart = cart
+    end
 end
