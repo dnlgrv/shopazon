98834b12af9605b2601feeea6484ad30a8b1876e
Daniel Grieve <dnlgrv@hey.com>
2021-09-17 12:53:27 +0100

    Remove products#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,4 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
-
-  def show
-    @product = Product.find(params[:id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :cart_products, only: [:create, :destroy]
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index]
   root to: "demo#index"
 end
