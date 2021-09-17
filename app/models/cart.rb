95ab466ba97a89c17975766dd93a6794fa28ee89
Daniel Grieve <dnlgrv@hey.com>
2021-09-17 12:53:26 +0100

    Display number of products in cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,6 +4,10 @@ class Cart < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :products, through: :cart_products
 
+  def quantity_of(product)
+    cart_products.where(product: product).count
+  end
+
   def total_price
     products.sum(&:price)
   end
