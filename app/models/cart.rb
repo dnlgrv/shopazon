c882bf321502efac3733c76b3746e42fb405676a
Daniel Grieve <dnlgrv@hey.com>
2021-09-17 12:53:26 +0100

    Display total price in cart nav item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,8 @@ class Cart < ApplicationRecord
 
   has_many :cart_products, dependent: :destroy
   has_many :products, through: :cart_products
+
+  def total_price
+    products.sum(&:price)
+  end
 end
