331c0df1240649c2877489e465baf7c21b0b3215
Daniel Grieve <dnlgrv@hey.com>
2021-09-18 08:09:22 +0100

    Display empty cart message

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,6 +4,8 @@ class Cart < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :products, through: :cart_products
 
+  delegate :empty?, to: :cart_products
+
   def quantity_of(product)
     cart_products.where(product: product).count
   end
