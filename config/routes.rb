98e4ed10881756fd1e6d5af04a7c4fc12ae5056d
Daniel Grieve <dnlgrv@hey.com>
2021-09-20 20:47:23 +0100

    Add products#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :cart_products, only: [:create, :destroy]
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
   root to: "demo#index"
 end
